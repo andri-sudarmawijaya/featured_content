fc51fae3c9f0ea882b7b1cdfb48180c02e745b48
andri-sudarmawijaya <andri.sudarmawijaya@gmail.com>
2019-09-19 20:05:39 +0700

    debug

--- src/Plugin/Block/FeaturedContentBlock.php
+++ src/Plugin/Block/FeaturedContentBlock.php
@@ -25,7 +25,11 @@ class FeaturedContentBlock extends BlockBase {
     $view = \Drupal\views\Views::getView('front_services');
 	$view->setDisplay('default');
 	$view->preExecute();
-    //return $build;
+    $view->execute();
+
+	$render_view = $view->render();
+    dpm($render_view);
+    
 	return [
 		'#type' => 'view',
 		'#name' => 'myfirstview',
@@ -35,27 +39,3 @@ class FeaturedContentBlock extends BlockBase {
 	];
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function getData() {
-    $view = \Drupal\views\Views::getView('front_services');
-	$view->setDisplay('default');
-	$view->preExecute();
-
-    if ($view) {
-      $view->execute();
-      foreach ($view->result as $rid => $row) {
-        foreach ($view->field as $fid => $field ) {
-          $value[$rid][$fid . '-value'] = $field->getValue($row);
-        }
-      }
-	  https://api.drupal.org/api/drupal/core!modules!views!views.api.php/function/hook_views_pre_render/8.2.x
-    }
-	$render_view = $view->render();
-    dpm($render_view);
-	//dpm($value);
-	//dpm($view->result);
-    return $value;
-  }
-}
