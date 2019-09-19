4ff4e8419d4ef7cc23da51c3ee20f0ff0662d08c
andri-sudarmawijaya <andri.sudarmawijaya@gmail.com>
2019-09-19 11:26:17 +0700

    fix naming

---
+++ src/Plugin/Block/FeaturedContentBlock.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\featured_content\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'FeaturedContentBlock' block.
+ *
+ * @Block(
+ *  id = "featured_content_block",
+ *  admin_label = @Translation("Featured content block"),
+ * )
+ */
+class FeaturedContentBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['#theme'] = 'featured_content_block';
+     $build['featured_content_block']['#markup'] = 'Implement FeaturedContentBlock.';
+
+    return $build;
+  }
+
+}
