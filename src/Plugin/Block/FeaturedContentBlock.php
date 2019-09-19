4ab39ca86c8d9777cd8a772ae9a19ca7f7ca8dcb
andri-sudarmawijaya <andri.sudarmawijaya@gmail.com>
2019-09-19 18:37:17 +0700

    Please enter the commit message for your changes. Lines starting

--- src/Plugin/Block/FeaturedContentBlock.php
+++ src/Plugin/Block/FeaturedContentBlock.php
@@ -39,13 +39,14 @@ class FeaturedContentBlock extends BlockBase {
       foreach ($view->result as $rid => $row) {
         foreach ($view->field as $fid => $field ) {
           $value[$rid][$fid . '-value'] = $field->getValue($row);
-          $value[$rid][$fid . '-render'] = $field->advancedRender($row);
         }
       }
+	  https://api.drupal.org/api/drupal/core!modules!views!views.api.php/function/hook_views_pre_render/8.2.x
     }
-	
-	dpm($value);
-	dpm($view->result);
+	$render_view = $view->render();
+    dpm($render_view);
+	//dpm($value);
+	//dpm($view->result);
     return $value;
   }
 }
