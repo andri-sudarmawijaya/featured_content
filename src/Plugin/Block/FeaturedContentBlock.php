44b44b2ab02d966c126bf37356658c7a5a1ff562
andri-sudarmawijaya <andri.sudarmawijaya@gmail.com>
2019-09-19 13:02:01 +0700

    Please enter the commit message for your changes. Lines starting

--- src/Plugin/Block/FeaturedContentBlock.php
+++ src/Plugin/Block/FeaturedContentBlock.php
@@ -38,7 +38,7 @@ class FeaturedContentBlock extends BlockBase {
 	// $myresults = $view->preview();  = array
 	// $myresults = $view->render();  = array
 	$results = $view->result; // = array
-	dpm($results);
+	dpm($results[0]);
     return $results;
   }
 }
